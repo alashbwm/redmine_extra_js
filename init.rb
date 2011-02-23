1eb7853a1c92c7678e22e9c05eb60ed285cd9b41
Albert Lash <alash@bluewatermedia.com>
2011-02-23 12:33:06 -0500

    first commit

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_extra_js do
+  name 'Redmine Extra Js plugin'
+  author 'Albert Lash'
+  description 'This adds extra javascript'
+  version '0.0.1'
+  url 'https://github.com/alashbwm/redmine_extra_js'
+  author_url 'http://example.com/about'
+end
